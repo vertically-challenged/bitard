08f0a5e407f50c86eed5d0e9eb0a3ac7b05adc1e
Anton <anton2chewski@gmail.com>
2020-07-10 19:53:13 +0300

    fix

--- rotate.js
+++ rotate.js
@@ -1,12 +1,25 @@
-(function () {
-	var rotateY = -45,
-		  rotateX = 0;
+var rotateY = -45, rotateX = 0;
+var rotate = true; 
 
+function autoRotate () {
+	if (rotate) {rotateY += 1};
+
+	document.querySelector('.Bitard').style.transform = 
+	'rotateY(' + rotateY + 'deg)'+
+	'rotateX(' + rotateX + 'deg)';
+}
+
+(function () {
+	setInterval( autoRotate, 25);
+})();
+
+(function () {
 	document.onkeydown = function (e) {
-		if (e.keyCode === 37) rotateY -= 4
-		else if (e.keyCode === 38) rotateX += 4
-		else if (e.keyCode === 39) rotateY += 4
-		else if (e.keyCode === 40) rotateX -= 4
+		rotate = false;
+		if (e.keyCode === 37|| e.keyCode === 65) rotateY -= 4
+		else if (e.keyCode === 38 || e.keyCode === 87) rotateX += 4
+		else if (e.keyCode === 39 || e.keyCode === 68) rotateY += 4
+		else if (e.keyCode === 40 || e.keyCode === 83) rotateX -= 4
 
         document.querySelector('.Bitard').style.transform = 
       'rotateY(' + rotateY + 'deg)'+
